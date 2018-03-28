bebb9b264c5e19aa2fa8edfc282dad7ccc2d021f
Lance Wong <lwong180@gmail.com>
2018-03-28 03:51:35 +0000

    state: est. relationship w/ City

--- models/state.py
+++ models/state.py
@@ -2,8 +2,11 @@
 '''
     Implementation of the State class
 '''
+import os
+import models
 from models.base_model import BaseModel, Base
 from sqlalchemy import Table, Column, String
+from sqlalchemy.orm import relationship, backref
 
 
 class State(BaseModel):
@@ -12,3 +15,13 @@ class State(BaseModel):
     '''
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        cities = relationship("City",
+                              backref=("state", cascade="delete"))
+    else:
+        @property
+        def cities(self):
+            """gets list of City objs where state_id=State.id"""
+            city_dict = models.storage.all(City)
+            return [city for city in city_dict.values()
+                    if city.state_id == self.id]
