fc66c04d02c673a36150ebbfc582c842e048ebf3
Lance Wong <lwong180@gmail.com>
2018-03-28 06:06:39 +0000

    import relationship

--- models/city.py
+++ models/city.py
@@ -4,6 +4,7 @@
 '''
 from models.base_model import BaseModel, Base
 from sqlalchemy import Table, Column, ForeignKey, String
+from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
--- models/state.py
+++ models/state.py
@@ -6,7 +6,7 @@ import os
 import models
 from models.base_model import BaseModel, Base
 from sqlalchemy import Table, Column, String
-from sqlalchemy.orm import relationship, backref
+from sqlalchemy.orm import relationship
 
 
 class State(BaseModel):
