26efc9fafb9a4085bf4b257f0b8ecc61d3143954
imperfectskillz <j.choi.89@gmail.com>
2018-03-29 11:35:34 +0000

    1

---
+++ 1-pack_web_static.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+#fabric script generates a tgz archive
+from fabric.api import *
+from datetime import datetime
+
+
+def do_pack():
+    """
+    creates tgz
+    """
+    filetime = datetime.now().strftime('%Y%m%d%H%M%s')
+    filename = 'versions/web_static_{}.tgz'.format(filetime)
+    try:
+        local('tar -cvzf {} web_static'.format(filename))
+        return filename
+    except:
+        return None
