819d5d548091cc26a1194f35eb03a378e6040f3e
imperfectskillz <j.choi.89@gmail.com>
2018-03-29 10:22:42 +0000

    added sudo

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,14 +2,14 @@
 # prepares web servers
 sudo apt-get update
 sudo apg-get install nginx
-mkdir /data/
-mkdir /data/web_static/
-mkdir /data/web_static/releases/
-mkdir /data/web_static/shared/
-mkdir /data/web_static/releases/test/
-touch /data/web_static/releases/test/index.html
-echo "James CHoi" >> /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test /data/web_static/current
-chown -R ubuntu:ubuntu /data
-sed -i "39i \tlocation /hbnb_static {\n\talias /data/web_static/current/;\n\t}" /etc/nginx/sites-enabled/default
-service nginx restart
+sudo mkdir /data/
+sudo mkdir /data/web_static/
+sudo mkdir /data/web_static/releases/
+sudo mkdir /data/web_static/shared/
+sudo mkdir /data/web_static/releases/test/
+sudo touch /data/web_static/releases/test/index.html
+echo -e "James CHoi" >> /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data
+sudo sed -i "39i \tlocation /hbnb_static {\n\talias /data/web_static/current/;\n\t}" /etc/nginx/sites-enabled/default
+sudo service nginx restart
