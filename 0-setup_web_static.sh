5a3940b7231ad6e354125e1e0d0bf4d16c7e3dd5
Lance Wong <lwong180@gmail.com>
2018-04-02 21:24:33 +0000

    fix mkdir

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,13 +2,12 @@
 # sets up web servers for deployment of web_static
 sudo apt-get -y update
 sudo apt-get -y install nginx
-sudo mkdir -p data/web_static/releases/test data/web_static/shared
-sudo touch data/web_static/releases/test/index.html
+sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
+sudo touch /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data
-sudo sed -i '37i\
-\
-\tlocation /hbnb_static/ {\
-\talias /data/web_static/current/;\
-\t}' /etc/nginx/sites-available/default
-sudo service nginx start
+sudo sed -i '37i\'\
+'\tlocation /hbnb_static/ {\n'\
+'\talias /data/web_static/current/;\n'\
+'\t}' /etc/nginx/sites-available/default
+sudo service nginx restart
