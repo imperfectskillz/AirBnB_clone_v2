46f163fecc191cf3b3c0f829db66435962ef9bee
imperfectskillz <j.choi.89@gmail.com>
2018-04-03 04:19:40 +0000

    syntax

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -28,18 +28,16 @@ def do_deploy(archive_path):
     """
     file = archive_path.split("/")[1]
 
-    if os.path.isfile(archive_path):
-        try:
-            put(archive_path, "/tmp/{}".format(file))
-            file1 = file.split(".")[0]
-            run("mkdir -p /data/web_static/releases/()/".format(file1))
-            run("tar -xzf /tmp/{} -C /data/web_static/releases/{}".format(
-                file, file1))
-            run("rm /tmp/{}".format(file))
-            run("mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/".format(file1, file1))
-            run("rm -rf /data/web_static/releases/{}/web_static".format(file1))
-            run("rm -rf /data/web_static/current")
-            run("ln -s /data/web_static/releases/{}/ /data/web_static/current".format(file1))
-            return True
-        except:
-            return False
+    if os.path.exists(archive_path):
+        put(archive_path, "/tmp/")
+        file1 = file.split(".")[0]
+        run("mkdir -p /data/web_static/releases/{}/".format(file1))
+        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}".format(file, file1))
+        run("rm /tmp/{}".format(file))
+        run("cp /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/".format(file1, file1))
+        run("rm -rf /data/web_static/releases/{}/web_static".format(file1))
+        run("rm /data/web_static/current")
+        run("ln -sf /data/web_static/releases/{}/ /data/web_static/current".format(file1))
+        return True
+
+    return False
