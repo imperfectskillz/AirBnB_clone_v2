961bfdea1de304ad7e6548eaaa6c03b54f4004ea
imperfectskillz <j.choi.89@gmail.com>
2018-04-09 14:35:46 +0000

    wrong folder

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,14 +2,9 @@
 # prepares web servers
 sudo apt-get update
 sudo apt-get install nginx -y
-sudo mkdir /data/
-sudo mkdir /data/web_static/
-sudo mkdir /data/web_static/releases/
-sudo mkdir /data/web_static/shared/
-sudo mkdir /data/web_static/releases/test/
-sudo touch /data/web_static/releases/test/index.html
+sudo mkdir -p /data/web_static/releases/test/ /data/web_static/shared/
 sudo echo -e "James CHoi" >> /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test /data/web_static/current
-sudo chown -R ubuntu:ubuntu /data
-sudo sed -i "39i \tlocation /hbnb_static {\n\talias /data/web_static/current/;\n\t}" /etc/nginx/sites-enabled/default
+sudo chown -hR ubuntu:ubuntu /data/
+sudo sed -i "38i \tlocation /hbnb_static {\n\talias /data/web_static/current;\n\t}" /etc/nginx/sites-available/default
 sudo service nginx restart
