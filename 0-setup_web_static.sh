8123798bf8fe2aa55a060dce122d9f81c6ee2f83
imperfectskillz <j.choi.89@gmail.com>
2018-04-09 14:25:37 +0000

    sudo

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -8,7 +8,7 @@ sudo mkdir /data/web_static/releases/
 sudo mkdir /data/web_static/shared/
 sudo mkdir /data/web_static/releases/test/
 sudo touch /data/web_static/releases/test/index.html
-echo -e "James CHoi" >> /data/web_static/releases/test/index.html
+sudo echo -e "James CHoi" >> /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data
 sudo sed -i "39i \tlocation /hbnb_static {\n\talias /data/web_static/current/;\n\t}" /etc/nginx/sites-enabled/default
--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -8,6 +8,7 @@ import os
 env.hosts = ["52.91.246.129", "107.23.155.217"]
 env.user = "ubuntu"
 
+
 def do_pack():
     """
     creates tgz
@@ -32,12 +33,14 @@ def do_deploy(archive_path):
         put(archive_path, "/tmp/")
         file1 = file.split(".")[0]
         run("mkdir -p /data/web_static/releases/{}/".format(file1))
-        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}".format(file, file1))
+        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}".format
+            (file, file1))
         run("rm /tmp/{}".format(file))
         run("cp /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/".format(file1, file1))
         run("rm -rf /data/web_static/releases/{}/web_static".format(file1))
         run("rm /data/web_static/current")
-        run("ln -sf /data/web_static/releases/{}/ /data/web_static/current".format(file1))
+        run("ln -sf /data/web_static/releases/{}/ /data/web_static/current".
+            format(file1))
         return True
 
     return False
